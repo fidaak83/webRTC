1716d7604a2a49bf2a324de4834902b3f2bc7086
fidaak83 <bdcrak-fidaak1983@yahoo.com>
2025-01-01 10:07:13 +0300

    updates server js

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.get('/', (req, res) => {
 });
 
 // Use environment variable for port, or fallback to 3000 if not set
-const PORT = 3000;
-
+// const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 // Start the server
 server.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
